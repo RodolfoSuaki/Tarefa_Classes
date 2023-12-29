5879d2fbab532b7125d20e3393920728e6b6e9d0
Rodolfo Suaki <rmsuaki@hotmail.com>
2023-12-29 01:12:05 -0300

    first commit

---
+++ src/Carro.java
@@ -0,0 +1,57 @@
+/*
+Comentário requerido na tarefa
+*/
+
+public class Carro {
+
+    private int velocidade;
+    private String nome;
+    private String cor;
+    private String modelo;
+    private int ano;
+
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+
+
+
+}
---
+++ src/MeiosDeTransporte.java
@@ -0,0 +1,8 @@
+public class MeiosDeTransporte {
+
+    public static void main (String args[]){
+        Carro carro = new Carro();
+        carro.setCor("Preto");
+        System.out.println(carro.getCor());
+    }
+}
